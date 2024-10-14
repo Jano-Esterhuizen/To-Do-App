234519df4e762e7c52eb1f0108ffc6bf411b5a78
Jano Personal <esterhuizenjano@gmail.com>
2024-10-14 17:27:09 +0200

    Navbar Profile card
    - Alo added Search bar Component file with no implimentation

--- frontend/To-Do-App/src/components/NavBar/NavBar.jsx
+++ frontend/To-Do-App/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import ProfileInfo from '../Cards/ProfileInfo';
+import { useNavigate } from 'react-router-dom';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate("/Login");
+  }
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+
+      <SearchBar />
+      <ProfileInfo onLogout={onLogout} />
+
     </div>
+
+    
   );
 };
 
