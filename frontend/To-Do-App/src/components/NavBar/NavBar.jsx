84d90910e081325104e45a1f41d9966cb917bf45
Jano Personal <esterhuizenjano@gmail.com>
2024-10-14 17:50:51 +0200

    Implemented Search Bar Component

--- frontend/To-Do-App/src/components/NavBar/NavBar.jsx
+++ frontend/To-Do-App/src/components/NavBar/NavBar.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileInfo from '../Cards/ProfileInfo';
 import { useNavigate } from 'react-router-dom';
 import { SearchBar } from '../SearchBar/SearchBar';
 
 const NavBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
 
   const onLogout = () => {
     navigate("/Login");
-  }
+  };
+
+  const handleSearch = () => {
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
+  
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-      <SearchBar />
+      <SearchBar value={searchQuery} 
+      onChange={({ target }) => {
+        setSearchQuery(target.value)
+      }} 
+      handleSearch={handleSearch}
+      onClearSearch={onClearSearch}
+      />
+
       <ProfileInfo onLogout={onLogout} />
 
     </div>
--- frontend/To-Do-App/src/components/SearchBar/SearchBar.jsx
+++ frontend/To-Do-App/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import {FaMagnifyingGlass} from 'react-icons/fa6'
+import { IoMdClose } from 'react-icons/io'
 
-export const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
+
+
+export const SearchBar = ({ value, onChange, handleSearch, onClearSearch }) => {
+    return (
+        <div className='w-80 flex items-center px-4 bg-slate-100 border-[1.5px] rounded-md'>
+            <input
+                type="text"
+                placeholder="Search Notes"
+                className="w-full text-xs bg-transparent py-[11px] outline-none"
+                value={value}
+                onChange={onChange}
+            />
+            
+
+            {value && (
+                <IoMdClose className='text-xl text-slate-400 cursor-pointer hover:text-black mr-3' onClick={onClearSearch}/>
+            )}
+
+            
+
+            <FaMagnifyingGlass className='text-slate-400 cursor-pointer hover:text-black' onClick={handleSearch}/>
+        </div>
+
+    )
 }
