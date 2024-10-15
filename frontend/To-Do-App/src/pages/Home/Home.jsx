b5975d9ef030b46d9250a9e00dc6eaabac6ed2dc
Jano Personal <esterhuizenjano@gmail.com>
2024-10-15 22:40:05 +0200

    Implemented Note Card
    - Created the NoteCard.jsx file and added it to the home screen
    
    - Added floating action button in the bottom right corner w.o functionality

--- frontend/To-Do-App/src/pages/Home/Home.jsx
+++ frontend/To-Do-App/src/pages/Home/Home.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md';
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      
+
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-3 gap-4 mt-8'>
+
+        <NoteCard
+          title="meeting" 
+          date="10/10/2022" 
+          content="content" 
+          tags={['tag1', 'tag2']} 
+          isPinned={false} 
+          onEdit={() => { }} 
+          onDelete={() => { }} 
+          onPinNote={() => { }}
+          />
+          
+        </div>
+      </div>
+
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute bottom-5 right-5 ' onClick={() => {}}>
+        <MdAdd className='text-[32px] text-white' />
+      </button>
     </>
-  )
-}
+  );
+};
 
 export default Home
