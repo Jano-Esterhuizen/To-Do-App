eee6f66a1acbd812409e8555eb6ae49c19c9466c
Jano Personal <esterhuizenjano@gmail.com>
2024-10-15 23:14:06 +0200

    Implimented Add/Edit Note Popup
    - Added "AddEditNote.jsx" file
    
    - Implimented Modal for popup using button click
    
    - Created input-label component

--- frontend/To-Do-App/src/pages/Home/Home.jsx
+++ frontend/To-Do-App/src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import NoteCard from '../../components/Cards/NoteCard'
 import { MdAdd } from 'react-icons/md';
+import AddEditNotes from './AddEditNotes';
+import Modal from 'react-modal';
 
 const Home = () => {
+
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
   return (
     <>
       <NavBar />
@@ -11,23 +20,46 @@ const Home = () => {
       <div className='container mx-auto'>
         <div className='grid grid-cols-3 gap-4 mt-8'>
 
-        <NoteCard
-          title="meeting" 
-          date="10/10/2022" 
-          content="content" 
-          tags={['tag1', 'tag2']} 
-          isPinned={false} 
-          onEdit={() => { }} 
-          onDelete={() => { }} 
-          onPinNote={() => { }}
+          <NoteCard
+            title="meeting"
+            date="10/10/2022"
+            content="content"
+            tags={['tag1', 'tag2']}
+            isPinned={false}
+            onEdit={() => { }}
+            onDelete={() => { }}
+            onPinNote={() => { }}
           />
-          
+
         </div>
       </div>
 
-      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute bottom-5 right-5 ' onClick={() => {}}>
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute bottom-5 right-5 ' 
+      onClick={() => {
+        setOpenAddEditModal({
+          isShown: true,
+          type: "add",
+          data: null,
+        });
+      }}>
+
         <MdAdd className='text-[32px] text-white' />
       </button>
+
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => { }}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes />
+      </Modal>
+
     </>
   );
 };
