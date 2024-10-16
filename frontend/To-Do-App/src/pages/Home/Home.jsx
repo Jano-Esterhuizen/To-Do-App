c77565498758f7cf6ab2c549eb591613bd1d54cc
Jano Personal <esterhuizenjano@gmail.com>
2024-10-16 16:18:32 +0200

    Get User API integration

--- frontend/To-Do-App/src/pages/Home/Home.jsx
+++ frontend/To-Do-App/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import NoteCard from '../../components/Cards/NoteCard'
 import { MdAdd } from 'react-icons/md';
 import AddEditNotes from './AddEditNotes';
 import Modal from 'react-modal';
+import axiosInstance from '../../utils/axiosInstance';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
 
@@ -13,9 +15,34 @@ const Home = () => {
     data: null,
   });
 
+  const [userInfo, setUserInfo] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Get User Info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+    return () => {};
+  }, []);
+
+
   return (
     <>
-      <NavBar />
+      <NavBar userInfo={userInfo} />
 
       <div className='container mx-auto'>
         <div className='grid grid-cols-3 gap-4 mt-8'>
