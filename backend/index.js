19850a89691198d5870264757cc6fd902b01d0ef
Jano Personal <esterhuizenjano@gmail.com>
2024-10-16 12:09:19 +0200

    Created Login endpoint

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,44 @@ app.post("/create-account", async (req, res) => {
 
 });
 
+//login
+app.post("/login", async (req, res) => {
+  
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Please enter your email" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ error: true, message: "Please enter your password" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(404).json({ error: true, message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "36000m"});
+    
+  
+
+  return res.json({ 
+    error: false,
+    user: user, 
+    email: email,
+    accessToken: accessToken, 
+    message: "Login successful" });
+
+  } else {
+    return res.status(400).json({ error: true, message: "Invalid credentials" });
+  }
+  
+});
+
 app.listen(8000);
 
 module.exports = app;
