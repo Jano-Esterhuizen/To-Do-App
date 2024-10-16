9842a6ff396c5c9136861f55aeefc8efcab02f42
Jano Personal <esterhuizenjano@gmail.com>
2024-10-16 11:55:05 +0200

    Implemented Account API
    - Added Create Account Model
    - Created "create-user" endpoint in index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString)
+
+const User = require("./models/user.models");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
@@ -12,6 +24,47 @@ app.use(
 
 app.get("/", (req, res) => {
   res.json({ data: "hello" });
+});
+
+//create account
+app.post("/create-account", async (req, res) => {
+
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res.status(404).json({ error: true, message: "Please enter your full name" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Please enter your email" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ error: true, message: "Please enter your password" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res.json({ error: true, message: "User already exists" });
+  }
+
+  const user = new User({
+    fullName: fullName,
+    email: email,
+    password: password
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "36000m"});
+
+  return res.json({ 
+    error: false,
+    user: user, 
+    accessToken: accessToken, 
+    message: "Account created successfully" });
+
 });
 
 app.listen(8000);
